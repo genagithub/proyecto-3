963a41678fa8352f1de6be40239b8f3d9b5ee51c
genagithub <143031972+genagithub@users.noreply.github.com>
2026-02-18 16:14:50 -0300

    commit

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ app = dash.Dash(__name__)
 server = app.server
 
 app.layout = html.Div(id="body",children=[
-    html.H1("Inteligencia de negocios aplicada en ventas",className="e3_title",href="https://github.com/genagithub/proyecto-3/blob/main/inteligencia_de_negocios_aplicada_en_ventas.ipynb",target="blank"),
+    html.A(href="https://github.com/genagithub/proyecto-3/blob/main/inteligencia_de_negocios_aplicada_en_ventas.ipynb",target="blank",children=[html.H1("Inteligencia de negocios aplicada en ventas",id="title",className="e3_title")]),
     html.Div(id="dropdown_div",className="e3_dropdown_div",children=[
             dcc.Dropdown(id="dropdown",className="e3_dropdown",
                         options = [

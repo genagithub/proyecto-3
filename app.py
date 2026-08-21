dbfcae921a28ca3591d66943be369f8fbdbe998b
genagithub <143031972+genagithub@users.noreply.github.com>
2026-08-21 20:12:44 -0300

    commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from dash.dependencies import Input, Output
 
 with sqlite3.connect("data/AdventureWorks.db") as conn:
 
-    query_profability = """
+    query_category = """
     WITH LinearTransform AS (
     SELECT sod.SalesOrderID,
         sod.ProductID,
@@ -36,9 +36,9 @@ with sqlite3.connect("data/AdventureWorks.db") as conn:
     ORDER BY TotalNetProfit DESC;
     """
 
-    df_profability = pd.read_sql_query(query_profability, conn)
+    df_category = pd.read_sql_query(query_category, conn)
 
-    query_territory = """
+    query_sales_channel = """
     WITH TerritoryTransformation AS (
     SELECT
         CASE
@@ -63,7 +63,7 @@ with sqlite3.connect("data/AdventureWorks.db") as conn:
     ORDER BY NetProfit DESC;    
     """
 
-    df_territory = pd.read_sql_query(query_territory, conn)
+    df_sales_channel = pd.read_sql_query(query_sales_channel, conn)
 
     query_customers = """
     SELECT CustomerID,
@@ -82,43 +82,37 @@ with sqlite3.connect("data/AdventureWorks.db") as conn:
     
     df_customers = pd.read_sql_query(query_customers, conn)
 
-total_company_revenue = df_profability["TotalGrossIncome"].sum()
-total_company_net_profit = df_profability["TotalNetProfit"].sum()
+total_company_revenue = df_category["TotalGrossIncome"].sum()
+total_company_net_profit = df_category["TotalNetProfit"].sum()
 company_wide_net_margin_baseline = (total_company_net_profit / total_company_revenue) * 100
 
-top_volume_row = df_profability.sort_values(by="TotalSales", ascending=False).iloc[0]
-global_total_units = df_profability["TotalSales"].sum()
-top_volume_market_share = (top_volume_row["TotalSales"] / global_total_units) * 100
+top_volume_row = df_category.sort_values(by="TotalQuantity", ascending=False).iloc[0]
+global_total_units = df_category["TotalQuantity"].sum()
+top_volume_market_share = (top_volume_row["TotalQuantity"] / global_total_units) * 100
+top_revenue_row = df_category.sort_values(by="TotalGrossIncome", ascending=False).iloc[0]
+top_efficiency_row = df_category.sort_values(by="NetMarginPercentage", ascending=False).iloc[0]
 
-top_revenue_row = df_profability.sort_values(by="TotalGrossIncome", ascending=False).iloc[0]
-
-top_efficiency_row = df_profability.sort_values(by="NetMarginPercentage", ascending=False).iloc[0]
-
-total_company_orders = df_territory["TotalOrders"].sum()
-total_company_costs = df_territory["MonthlyCosts"].sum()
+total_company_orders = df_sales_channel["TotalOrders"].sum()
+total_company_costs = df_sales_channel["MonthlyCosts"].sum()
 company_cost_per_order_baseline = total_company_costs / total_company_orders
 
-top_regional_volume = df_territory.sort_values(by="TotalOrders", ascending=False).iloc
-
-top_regional_profit = df_territory.sort_values(by="NetProfit", ascending=False)
-
-df_territory["CostPerOrder"] = df_territory["MonthlyCosts"] / df_territory["TotalOrders"]
-top_cost_efficiency = df_territory.sort_values(by="CostPerOrder", ascending=True).iloc
+top_regional_volume = df_sales_channel.sort_values(by="TotalOrders", ascending=False).iloc
+top_regional_profit = df_sales_channel.sort_values(by="NetProfit", ascending=False)
+df_sales_channel["CostPerOrder"] = df_sales_channel["MonthlyCosts"] / df_sales_channel["TotalOrders"]
+top_cost_efficiency = df_sales_channel.sort_values(by="CostPerOrder", ascending=True).iloc
 
 total_spend_global = df_customers["TotalSpend"].sum()
 total_orders_global = df_customers["TotalOrders"].sum()
 company_wide_aov_baseline = total_spend_global / total_orders_global
 
 top_monetary_customer = df_customers.sort_values(by="TotalSpend", ascending=False).iloc[0]
-
 top_frequency_customer = df_customers.sort_values(by="TotalOrders", ascending=False).iloc[0]
-
 top_ticket_customer = df_customers.sort_values(by="AverageOrderValue", ascending=False).iloc[0]
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div(id="body",children=[
-    html.H1("AdventureWorks Analytics: Panel de control financiero y omnicanal de E-Commerce", className="e3_title", style={"margin-bottom":"50px"}),
+    html.H1("AdventureWorks Analytics: Panel de control financiero y omnicanal mensual", className="e3_title", style={"margin-bottom":"50px"}),
     html.Div(id="dropdown_div", className="e3_dropdown_div", children=[
             dcc.Dropdown(id="dropdown", className="e3_dropdown",
                         options = [
@@ -133,43 +127,43 @@ app.layout = html.Div(id="body",children=[
     dcc.Graph(id="figure-1", figure={}),
     html.H2("Palancas de negocio", className="e3_title"),
     html.Div(className="e3_container", children=[
-        html.Div(id="data_1", className="e3_children",style={"color":"blue"}, children=[
-            html.H2("Categorías", style={"font-size":"1.15em","color":"blue","font-family":"sans-serif"}),
-            html.P(f"Promedio (Margen Neto): {company_wide_net_margin_baseline}", className="e3_mean", style={"color":"blue"}),
-            html.Ul(className="e3_ul", style={"color":"blue"}, children=[
-                html.Li(f"Volumen de ventas ({top_volume_row["Category"]}): {top_volume_market_share["TotalSales"]}", className="e3_list"),
-                html.Li(f"Ingresos brutos ({top_revenue_row["Category"]}): {top_revenue_row["TotalGrossIncome"]}$", className="e3_list"),
-                html.Li(f"Margen Neto ({top_efficiency_row["Category"]}): {top_efficiency_row["NetMarginPercentage"]}", className="e3_list")
+        html.Div(id="data_1", className="e3_children", children=[
+            html.H2("Categorías", style={"font-size":"1.15em","font-family":"sans-serif"}),
+            html.P(f"Promedio (Margen Neto): {company_wide_net_margin_baseline}%", className="e3_mean"),
+            html.Ul(className="e3_ul", children=[
+                html.Li(f"Volumen de ventas ({top_volume_row["Category"]}): {top_volume_market_share["TotalQuantity"]} uds.", className="e3_list"),
+                html.Li(f"Ingresos Brutos ({top_revenue_row["Category"]}): ${top_revenue_row["TotalGrossIncome"]}", className="e3_list"),
+                html.Li(f"Margen Neto ({top_efficiency_row["Category"]}): {top_efficiency_row["NetMarginPercentage"]}%", className="e3_list")
             ])
         ]),
         html.Div(id="data_2", className="e3_children", children=[
-            html.H2("Territorios", style={"font-size":"1.15em","color":"red","font-family":"sans-serif"}),
-            html.P(f"Promedio (CPO): {company_cost_per_order_baseline}", className="e3_mean", style={"color":"red"}),
-            html.Ul(className="e3_ul", style={"color":"red"}, children=[
+            html.H2("Canal de ventas", style={"font-size":"1.15em","font-family":"sans-serif"}),
+            html.P(f"Promedio (CPO): ${company_cost_per_order_baseline}", className="e3_mean"),
+            html.Ul(className="e3_ul", children=[
                 html.Li(f"Volumen de órdenes ({top_regional_volume["SalesChannel"]}): {top_regional_volume["TotalOrders"]}", className="e3_list"),
-                html.Li(f"Ganancia Neta ({top_regional_profit["SalesChannel"]}): {top_regional_profit["NetProfit"]}", className="e3_list"),
+                html.Li(f"Ganancia Neta ({top_regional_profit["SalesChannel"]}): ${top_regional_profit["NetProfit"]}", className="e3_list"),
                 html.Li(f"Eficiencia de costos ({top_cost_efficiency["SalesChannel"]}): {top_cost_efficiency["CostPerOrder"]}", className="e3_list")
             ])
         ]),
         html.Div(id="data_3", className="e3_children", children=[
-            html.H2("Clientes", style={"font-size":"1.15em","color":"green","font-family":"sans-serif"}),
-            html.P(f"Promedio (AOV): {company_wide_aov_baseline}", className="e3_mean", style={"color":"green"}),
-            html.Ul(className="e3_ul",style={"color":"green"}, children=[
-                html.Li(f"Total gastado ({top_monetary_customer["CustomerID"]}): {top_monetary_customer["TotalSpend"]}", className="e3_list"),
+            html.H2("Clientes", style={"font-size":"1.15em","font-family":"sans-serif"}),
+            html.P(f"Promedio (AOV): ${company_wide_aov_baseline}", className="e3_mean"),
+            html.Ul(className="e3_ul", children=[
+                html.Li(f"Gasto total ({top_monetary_customer["CustomerID"]}): ${top_monetary_customer["TotalSpend"]}", className="e3_list"),
                 html.Li(f"Frecuencia ({top_frequency_customer["CustomerID"]}): {top_frequency_customer["TotalOrders"]}", className="e3_list"),
-                html.Li(f"Valor promedio de órden ({top_ticket_customer["CustomerID"]}): {top_ticket_customer["AverageOrderValue"]}", className="e3_list")
+                html.Li(f"Valor promedio de órden ({top_ticket_customer["CustomerID"]}): ${top_ticket_customer["AverageOrderValue"]}", className="e3_list")
             ])
         ])
     ]),
     html.Div(id="dropdown_2_div", className="e3_div_dropdown", children=[
         dcc.Dropdown(id="dropdown_category", className="e3_dropdown",
-                    options=df_profability["Category"].tolist(),
-                    value=df_profability["Category"].iloc[0],
+                    options=df_category["Category"].tolist(),
+                    value=df_category["Category"].iloc[0],
                     multi=False,
                     clearable=False),
         dcc.Dropdown(id="dropdown_sales_channel", className="e3_dropdown",
-                    options=df_territory["SalesChannel"].tolist(),
-                    value=df_territory["SalesChannel"].iloc[0],
+                    options=df_sales_channel["SalesChannel"].tolist(),
+                    value=df_sales_channel["SalesChannel"].iloc[0],
                     multi=False,
                     clearable=False),
         dcc.Dropdown(id="dropdown_customer", className="e3_dropdown",
@@ -190,8 +184,9 @@ app.layout = html.Div(id="body",children=[
     [Input(component_id="dropdown", component_property="value"),
     Input(component_id="dropdown_category", component_property="value"),
     Input(component_id="dropdown_sales_channel", component_property="value"),
-    Input(component_id="dropdown_customer" component_property="value")]
+    Input(component_id="dropdown_customer", component_property="value")]
 )
+
 
 def update_dashboard(slct_data, slct_category, slct_sales_channel, slct_customer):
 
@@ -199,14 +194,88 @@ def update_dashboard(slct_data, slct_category, slct_sales_channel, slct_customer
     sales_channel_style = {"position":"absolute","top":"0","left":"0"}
     customer_style = {"position":"absolute","top":"0","left":"0"}
 
-    # if slct_data == "Category":
+    figure_1 = go.Figure()
+    figure_2 = go.Figure()
 
+    if slct_data == "Category":
 
-    # elif slct_data == "Country":
+        category_style["zIndex"] = 5
 
+        figure_1 = px.bar(
+            df_category,
+            x="Category",
+            y=["TotalGrossIncome", "TotalProfitNet"],
+            barmode="group",
+            title="Ingreso Bruto vs Ganancia Neta",
+            labels={"value": "Amount ($)", "variable": "Métrica Financiera", "Category": "Categoría"},
+            template="plotly_dark"
+        )
 
-    # elif slct_data == "CustomerID":
+        df_filtered = df_category[df_category["Category"] == slct_category]
+        row = df_filtered.iloc[0]
 
+        figure_2 = px.pie(
+              names=["Costo de Producción", "Costo de Flete", "Costo de Impuesto", "Ganancia Neta"],
+              values=[row["TotalProductionCost"], row["TotalFreightCost"], row["TotalTaxCost"], row["TotalNetProfit"]],
+              title=f"Descomposición Financiera: {slct_category} (Margen Neto de Junio: {row["NetMarginPercentage"]:.1f}%)",
+              template="plotly_dark",
+              hole=0.4,
+              color_discrete_sequence=px.colors.qualitative.Pastel
+        )
+
+    elif slct_data == "SalesChannel":
+
+        sales_channel_style["zIndex"] = 5
+
+        figure_1 = px.bar(
+            df_sales_channel,
+            x="SalesChannel",
+            y="MonthlySales",
+            color="Country",
+            barmode="group",
+            title="Ventas Mensuales por Canal y Territorio",
+            labels={"MonthlySales": "Ventas ($)", "SalesChannel": "Canal de Ventas"},
+            template="plotly_dark"
+        )
+
+        df_filtered = df_sales_channel[df_sales_channel["SalesChannel"] == slct_sales_channel]
+
+        figure_2 = px.bar(
+            df_filtered,
+            x="Country",
+            y="NetMarginPercentage",
+            title=f"Eficiencia del Margen Neto por País para el Canal: {slct_sales_channel}",
+            labels={"NetMarginPercentage": "Margen Neto (%)", "Country": "País"},
+            template="plotly_dark"
+        ).update_traces(marker_color="#34d399")
+
+    elif slct_data == "CustomerID":
+
+        customer_style["zIndex"] = 5
+
+        figure_1 = px.bar(
+            df_customers,
+            x="CustomerID",
+            y="TotalSpend",
+            color="SalesChannel",
+            title="Top 10 Clientes VIP por Gasto Total Acumulado",
+            labels={"TotalSpend": "Gasto Total ($)", "CustomerID": "ID"},
+            template="plotly_dark"
+        )
+
+        df_filtered = df_customers[df_customers["CustomerID"] == int(slct_customer)]
+        row = df_filtered.iloc[0]
+
+        figure_2 = px.bar(
+              x=["Gasto Total", "Valor de Ticket Promedio (AOV)"],
+              y= [row["TotalSpend"], row["AverageOrderValue"]],
+              title=f"Perfil Financiero Individual del Cliente ID: {slct_customer}",
+              labels={"x": "Métrica Comercial", "y": "Valor ($)"},
+              template="plotly_dark"
+        ).update_traces(marker_color="#10b981")
+
+    else:
+        figure_2 = go.Figure()
 
     return figure_1, category_style, sales_channel_style, customer_style, figure_2
 

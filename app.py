7335cef47a08cded13d55017b51fbb422a79932b
genagithub <143031972+genagithub@users.noreply.github.com>
2026-08-22 19:56:56 -0300

    commit

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ with sqlite3.connect("data/AdventureWorks.db") as conn:
     SUM(Quantity) AS TotalQuantity,
     ROUND(SUM(GrossIncome), 2) AS TotalGrossIncome,
     ROUND(SUM(ProductionCost), 2) AS TotalProductionCost,
-    ROUND(SUM(FregihtCost), 2) AS TotalFreightCost,
+    ROUND(SUM(FreightCost), 2) AS TotalFreightCost,
     ROUND(SUM(TaxCost), 2) AS TotalTaxCost,
     ROUND(SUM(GrossIncome - ProductionCost - FregihtCost - TaxCost), 2) AS TotalNetProfit,
     ROUND((SUM(GrossIncome - ProductionCost - FregihtCost - TaxCost) / SUM(GrossIncome)) * 100, 2) AS NetMarginPercentage
